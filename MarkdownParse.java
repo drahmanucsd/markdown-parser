f3635973bfe4632bc880eb029284683e4daf9f83
drahmanucsd <103224717+drahmanucsd@users.noreply.github.com>
2022-04-14 17:19:15 -0700

    make sure the dist bet[] and() not 2 big

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,17 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-        // while(markdown.indexOf("[",currentIndex)!=-1) {
+        //while(currentIndex < markdown.length()) {
+        while(markdown.indexOf("[",currentIndex)!=-1) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            if(openBracket+10>openParen){
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                System.out.println(currentIndex);
+            }
         }
 
         return toReturn;
--- ptest3.md
+++ ptest3.md
@@ -1,3 +1,3 @@
 # Title
 
-dsfsd
+[link1](https://some()thing.com)
